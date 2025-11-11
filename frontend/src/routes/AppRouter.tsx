38e86a943538eaf65bb592380aed03a969bb9626
MegaNoris <lv53154@fer.hr>
2025-11-12 00:56:52 +0100

    Portofolio
    Napravljen kostur za gledanje portofolio user-a, podjeljen u grid, integracija sa backend-om se mora testirati.

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import StockView from "../pages/StockView";
 
 const AppRouter: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/view" element={<StockView />} />
       </Routes>
     </Router>
   );

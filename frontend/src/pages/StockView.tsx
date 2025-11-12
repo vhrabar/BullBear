516319764046fe76c48612579a6c0fa903862ecf
MegaNoris <lv53154@fer.hr>
2025-11-12 23:24:43 +0100

    Pretvoreno u tablicu
    (inicijalna verzija)

--- frontend/src/pages/StockView.tsx
+++ frontend/src/pages/StockView.tsx
@@ -3,10 +3,35 @@ import './StockView.css';
 import Footer from '../components/Footer';
 import { Link } from "react-router-dom";
 
+
+let test = JSON.parse('['+
+    '{'+
+        '"id": 1,'+
+        '"instrument": "AMD (AMD)",'+
+        '"current_value": "31014.000",'+
+        '"profit_loss": "294.000",'+
+        '"quantity": "120.000",'+
+        '"average_price": "256.000",'+
+        '"added_at": "2025-11-03T16:50:25.5999",'+
+        '"portofolio": 1'+
+    '},'+
+    '{'+
+        '"id": 2,'+
+        '"instrument": "NVDA(Nvidia Corporation)",'+
+        '"current_value": "25721.760",'+
+        '"profit_loss": "137.760",'+
+        '"quantity": "123.000",'+
+        '"average_price": "208.000",'+
+        '"added_at": "2025-11-03T16:57:07.7827",'+
+        '"portofolio": 1'+
+    '}'+
+']');
+
 function StockView() {
     const [content, setContent] = useState<JSX.Element>(<div>Loading...</div>);
 
-    const API_URL = "http://localhost:8000/api/trading/portfolio-holdings/";
+    // const API_URL = "http://localhost:8000/api/trading/portfolio-holdings/";
+    const API_URL = "/api/trading/portfolio-holdings/"; // provjera proxy servera
 
     useEffect(() => {
         fetch(API_URL, {
@@ -36,23 +61,73 @@ function StockView() {
                 quantity: string;
                 average_price: string;
             }) => (
-                <div key={x.id} className="stock-item">
-                    <div className="instrument">{x.instrument}</div>
-                    <div className="details">
-                        <p>Current Value: {x.current_value}</p>
-                        <p>Profit/Loss: {x.profit_loss}</p>
-                        <p>Quantity: {x.quantity}</p>
-                        <p>Average Price: {x.average_price}</p>
-                    </div>
-                </div>
+                <tr key={x.id} className="stock-item">
+                    <td>
+                        <p>{x.instrument.split('(')[0]}</p>
+                        <p className='corpo'>{x.instrument.split('(')[1].split(')')[0]}</p>
+                    </td>
+                    <td>{x.current_value}</td>
+                    <td style={(x.profit_loss[0]=='-')? {color: 'red'}:{color: 'green'}}>{x.profit_loss}</td>
+                    <td>{x.quantity}</td>
+                    <td>{x.average_price}</td>
+                </tr>
             ));
 
-            setContent(<div id='container'>{list}</div>);
+            setContent(
+            <div id='container-root'>
+                <table id='container'>
+                    <tr>
+                        <th>Instrument</th>
+                        <th>Current Value</th>
+                        <th>Profit/Loss</th>
+                        <th>Quantity</th>
+                        <th>Average Price</th>
+                    </tr>
+                    {list}
+                </table>
+            </div>);
         })
         .catch((error) => {
             setContent(<div id='error'>Error loading data: {error.message}</div>);
         });
     }, [API_URL]);
+
+
+    //TEST VERZIJA- DO NOT DELETE!!! --- lakšse za testiranje
+    // useEffect(() => {
+    //     const list = test.map((x: {
+    //             id: number;
+    //             instrument: string;
+    //             current_value: string;
+    //             profit_loss: string;
+    //             quantity: string;
+    //             average_price: string;
+    //         }) => (
+    //             <tr key={x.id} className="stock-item">
+    //                 <td>
+    //                     <p>{x.instrument.split('(')[0]}</p>
+    //                     <p className='corpo'>{x.instrument.split('(')[1].split(')')[0]}</p>
+    //                 </td>
+    //                 <td>{x.current_value}</td>
+    //                 <td style={(x.profit_loss[0]=='-')? {color: 'red'}:{color: 'green'}}>{x.profit_loss}</td>
+    //                 <td>{x.quantity}</td>
+    //                 <td>{x.average_price}</td>
+    //             </tr>
+    //         ));
+    //         setContent(
+    //         <div id='container-root'>
+    //             <table id='container'>
+    //                 <tr>
+    //                     <th>Instrument</th>
+    //                     <th>Current Value</th>
+    //                     <th>Profit/Loss</th>
+    //                     <th>Quantity</th>
+    //                     <th>Average Price</th>
+    //                 </tr>
+    //                 {list}
+    //             </table>
+    //         </div>);
+    // }, []);
 
     return (
         <div>

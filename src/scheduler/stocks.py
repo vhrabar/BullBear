7c859f361bd3fb9f56defc5dd2301f7221341a7a
vhrabar <vedran.hrabar@fer.hr>
2025-12-11 18:31:49 +0100

    add additional stock ticker symbols for subscription

--- src/scheduler/stocks.py
+++ src/scheduler/stocks.py
@@ -1,13 +1,46 @@
 
-# List of stock ticker symbols for subscription (US market only)
+# List of stock ticker symbols for subscription
 STOCKS = [
     "AMD", # Advanced Micro Devices
     "NVDA", # NVIDIA Corporation
     "ASML", # ASML Holding N.V.
+    "TSM", # Taiwan Semiconductor Manufacturing Company Limited
     "INTC", # Intel Corporation
     "SFTBY", # SoftBank Group Corp.
     "ARM", # Arm Holdings plc
     "TXN", # Texas Instruments Incorporated
+    "DTE", # Deutsche Telekom AG
+    "RHM", # Rheinmetall AG
+    "VOW", # Volkswagen AG
+
+    "AAPL", # Apple Inc.
+    "MSFT", # Microsoft Corporation
+    "AMZN", # Amazon.com, Inc.
+    "GOOGL", # Alphabet Inc. (Class A)
+    "AVGO", # Broadcom Inc.
+    "GOOG", # Alphabet Inc. (Class C)
+    "META", # Meta Platforms, Inc.
+    "TSLA", # Tesla, Inc.
+    "BRK.B", # Berkshire Hathaway Inc. (Class B)
+    "WMT", # Walmart Inc.
+    "LLY", # Eli Lilly and Company
+    "JPM", # JPMorgan Chase & Co.
+    "V", # Visa Inc.
+    "ORCL", # Oracle Corporation
+    "XOM", # Exxon Mobil Corporation
+    "NFLX", # Netflix, Inc.
+    "BAC", # Bank of America Corporation
+    "IBM", # International Business Machines Corporation
+    "MS", # Morgan Stanley
+    "GS", # The Goldman Sachs Group, Inc.
+    "DIS", # The Walt Disney Company
+    "QCOM", # QUALCOMM Incorporated
+    "SCHW", # The Charles Schwab Corporation
+    "BLK", # BlackRock, Inc.
+    "DELL", # Dell Technologies Inc.
+    "ADSK", # Autodesk, Inc.
+
+
 ]
 
 def get_subscribed_stocks() -> list[str]:

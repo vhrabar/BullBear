9bb72f73827dd6e0728a4679be639ee5e5da91dc
vhrabar <vedran.hrabar@fer.hr>
2025-12-11 15:45:08 +0100

    refactor main runner with test data for AMD #19

--- src/scheduler/runner.py
+++ src/scheduler/runner.py
@@ -1,16 +1,28 @@
-import asyncio
+# runner.py
+from massive import WebSocketClient
+from massive.websocket.models import Feed, Market
+from service import MinuteAggregateIngestionService
+from configuration import settings
 
-from configuration import Settings
 
+def main():
+    print("Starting Massive ingestion client...")
 
-async def main():
-    settings = Settings()
-    service = None
-    client = None
+    if settings.TEST_MODE:
+        print("TEST_MODE = TRUE — No DB writes will occur.")
+
+    service = MinuteAggregateIngestionService()
+
+    client = WebSocketClient(
+        api_key=settings.MASSIVE_API_KEY,
+        feed=Feed.Delayed,
+        market=Market.Stocks,
+    )
+
+    client.subscribe("AM.AMD")
+
+    client.run(service.handle_messages)
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except:
-        pass
+    main()

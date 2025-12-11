3aca014adc599aa3d87fff40155eb78388f924a8
vhrabar <vedran.hrabar@fer.hr>
2025-12-11 16:12:41 +0100

    minor refactoring #19

--- src/scheduler/repo.py
+++ src/scheduler/repo.py
@@ -1,6 +1,10 @@
+import traceback
 from datetime import datetime
+from typing import Any
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker, InstrumentedAttribute
 from sqlalchemy.dialects.postgresql import insert
 from orm_model import InstrumentQuote, InstrumentIntervalData, Instrument
 from configuration import settings
@@ -11,27 +15,47 @@ SessionLocal = sessionmaker(bind=engine)
 
 
 class MarketDataRepository:
-    def load_instrument_map(self) -> dict[str, int]:
+    @staticmethod
+    def load_instrument_map() -> dict[Any, InstrumentedAttribute[int]]:
         with SessionLocal() as session:
             rows = session.query(Instrument).filter(Instrument.is_active == True).all()
             return {row.symbol.upper(): row.id for row in rows}
 
-    def upsert_quote(self, payload: dict):
-        with SessionLocal() as session:
-            stmt = insert(InstrumentQuote).values(payload)
-            stmt = stmt.on_conflict_do_update(
-                index_elements=["instrument"],
-                set_=payload
-            )
-            session.execute(stmt)
-            session.commit()
+    @staticmethod
+    def upsert_quote(payload: dict):
+        try:
+            with SessionLocal() as session:
+                stmt = insert(InstrumentQuote).values(payload)
+                stmt = stmt.on_conflict_do_update(
+                    index_elements=["instrument"],
+                    set_=payload
+                )
 
-    def upsert_candle(self, payload: dict):
-        with SessionLocal() as session:
-            stmt = insert(InstrumentIntervalData).values(payload)
-            stmt = stmt.on_conflict_do_update(
-                index_elements=["instrument_id", "start_time"],
-                set_=payload
-            )
-            session.execute(stmt)
-            session.commit()
+                session.execute(stmt)
+                session.commit()
+
+        except SQLAlchemyError as exc:
+            print("\n[ERROR] Quote UPSERT failed.")
+            print("[PAYLOAD]:", payload)
+            print(type(exc).__name__, str(exc))
+            traceback.print_exc()
+
+
+    @staticmethod
+    def upsert_candle(payload: dict):
+        try:
+            with SessionLocal() as session:
+                stmt = insert(InstrumentIntervalData).values(payload)
+                stmt = stmt.on_conflict_do_update(
+                    index_elements=["instrument_id", "start_time"],
+                    set_=payload
+                )
+
+                session.execute(stmt)
+                session.commit()
+
+        except SQLAlchemyError as exc:
+            print("\n[ERROR] Candle UPSERT failed.")
+            print("[PAYLOAD]:", payload)
+            print(type(exc).__name__, str(exc))
+            traceback.print_exc()

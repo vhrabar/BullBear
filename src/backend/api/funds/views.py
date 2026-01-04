f8bf2b7e8ebcd21340030b9eb79c83465905dd2b
vhrabar <vedran.hrabar@fer.hr>
2026-01-04 16:12:23 +0100

    add serializers and viewsets for fund management #18

--- src/backend/api/funds/views.py
+++ src/backend/api/funds/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Fund, FundSubscription
+from .serializers import FundSerializer, FundSubscriptionSerializer
 
-# Create your views here.
+
+class FundViewSet(viewsets.ModelViewSet):
+    queryset = Fund.objects.all()
+    serializer_class = FundSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Fund.objects.filter(creator_portfolio__user=self.request.user)
+
+
+class FundSubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = FundSubscription.objects.all()
+    serializer_class = FundSubscriptionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return FundSubscription.objects.filter(subscriber_portfolio__user=self.request.user)
+
+

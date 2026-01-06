7a27a2f607c6f98a47ec4b084428adb02a61a73a
Antun Silov <antun.silov@fer.unizg.hr>
2026-01-06 18:02:38 +0100

    Implement importing and exporting transactions through CSV files #20
    # Conflicts:
    #	src/backend/api/users/urls.py
    #	src/backend/api/users/views.py

--- src/backend/api/users/urls.py
+++ src/backend/api/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserPortfolioViewSet, UserProfileViewSet
+from .views import UserPortfolioViewSet, export_csv, import_csv, UserProfileViewSet
 
 router = DefaultRouter()
 router.register(r'portofolio-details', UserPortfolioViewSet, basename='portfolio-details')
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('', include('allauth.socialaccount.urls')),
-
+    path('export/', export_csv, name="export"),
+    path('import/', import_csv, name="import")
 ]

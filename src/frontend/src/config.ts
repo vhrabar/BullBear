cebbb03eb5725561576d2690edee2524f6d650fe
vhrabar <vedran.hrabar@fer.hr>
2026-01-23 22:29:34 +0100

    minor fixes

--- src/frontend/src/config.ts
+++ src/frontend/src/config.ts
@@ -1,17 +1,17 @@
-import dotenv from "dotenv";
+let dotenv;
+if (typeof process !== "undefined" && process.versions != null && process.versions.node != null) {
+  dotenv = await import("dotenv");
+  dotenv.config();
+}
 
-dotenv.config();
-
-const env = process.env.NODE_ENV?.trim() || "development";
+const env = typeof process !== "undefined" && process.env?.NODE_ENV?.trim() || "development";
 
 export const MICROSOFT_OAUTH_URL =
   env === "production"
-    ? process.env.MICROSOFT_OAUTH_URL_PROD
-    : process.env.MICROSOFT_OAUTH_URL_DEV;
+    ? "https://api.bull-bear.app/auth/microsoft/login/"
+    : "http://localhost:8000/auth/microsoft/login/";
 
 export const GOOGLE_OAUTH_URL =
   env === "production"
-    ? process.env.GOOGLE_OAUTH_URL_PROD
-    : process.env.GOOGLE_OAUTH_URL_DEV;
-
-export const IS_PRODUCTION = env === "production";
+    ? "https://api.bull-bear.app/auth/google/login/"
+    : "http://localhost:8000/auth/google/login/";
